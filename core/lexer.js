b99c376230800e91c6f95bd6c4c945c9d912b352
Hamza Baig <hamzabaig18@gmail.com>
2017-04-20 00:20:00 +0500

    possibly completed the lexer wtf

--- core/lexer.js
+++ core/lexer.js
@@ -1,13 +1,27 @@
 const operators = ['|+', '|-', '|*', '|/', '|%', '<>', '!', '|&', '|#', '[', ']', '(', ')'];
 const keywords = ['var', 'fi', 'elif', 'loop', 'digit', 'rational', 'acquire', 'display'];
+const symbols = [' ', '=', ':', ',', ';'];
 const alphabets = 'abcdefghijklmnopqrstuvwxyz'
 const output = { operators: [], keywords: [] };
 function isOperator(string) {
   return operators.indexOf(string) > -1;
 }
 
+function isSymbol(string) {
+  return symbols.indexOf(string) > -1;
+}
+
 function isKeyword(string) {
   return keywords.indexOf(string) > -1;
+}
+
+function addToOutput(string) {
+  if (!string) return;
+  if (isOperator(string)) {
+    output.operators.push(string);
+  } else if(isKeyword(string)) {
+    output.keywords.push(string);
+  }
 }
 
 function analyse(string) {
@@ -15,9 +29,24 @@ function analyse(string) {
   let currentLine;
   for(let i=0; i < splitByDot.length; i++) {
     currentLine = splitByDot[i];
-    let splitedLine = currentLine.split(':'); // to check if declaration
-    if (splitedLine.length > 1) { // iden
-
+    if (currentLine.length < 2) {
+      output.operators.push(currentLine);
+      continue;
+    }
+    let lastIdentified = '';
+    for (let j=0;j < currentLine.length; j++) {
+      let currentChar = currentLine[j];
+      if (isSymbol(currentChar)) {
+        addToOutput(lastIdentified);
+        lastIdentified = '';
+        continue;
+      } else if (isOperator(currentChar)) {
+        output.operators.push(currentChar);
+        addToOutput(lastIdentified);
+        lastIdentified = '';
+        continue; // if no continue then it'll add all chars to lastIdentified
+      }
+      lastIdentified += currentChar;
     }
   }
 }
